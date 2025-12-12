b72ba2ce0ce0fb02f078b5a477eead0c658c98d3
Darshil Shah <darshils99@gmail.com>
2025-12-12 17:12:48 -0600

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_MAX_FILE_SIZE?: string;
+  readonly VITE_MAX_FILES?: string;
+  readonly VITE_RATE_LIMIT?: string;
+  readonly VITE_DEBUG?: string;
+  // Add more VITE_ prefixed env variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
