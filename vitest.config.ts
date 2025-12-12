97c9fb443cd31361ba9f0e05374aae8773dcc924
Darshil Shah <darshils99@gmail.com>
2025-12-12 17:14:14 -0600

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react() as any],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/vitest.setup.ts",
-
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      exclude: [
+        "node_modules/",
+        "src/tests/",
+        "**/*.test.{ts,tsx}",
+        "**/*.config.{ts,js}",
+      ],
+    },
   },
 });
