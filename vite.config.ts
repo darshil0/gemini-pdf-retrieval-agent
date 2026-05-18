5d2e6f2918955b1efc0ddb542f03d87fce0cd712
Darshil Shah <darshils99@gmail.com>
2026-05-18 16:40:46 -0500

    Replace path aliases with vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  
+
   return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "@api": path.resolve(__dirname, "./src/api"),
-        "@core": path.resolve(__dirname, "./src/core"),
-        "@components": path.resolve(__dirname, "./src/components"),
-        "@styles": path.resolve(__dirname, "./src/styles"),
-        "@tests": path.resolve(__dirname, "./src/tests"),
-      },
-    },
+    plugins: [react(), tsconfigPaths()],
     server: {
       port: parseInt(env.VITE_PORT || "5173"),
       host: true,
